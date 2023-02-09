d5d9a2ecbf41be6ebcac9dd99092d4a6495cdcbe
Ashok Basnet <ashokbasnetofficial@gmail.com>
2023-02-09 23:31:46 +0545

    todo update!

--- Todo List/main.js
+++ Todo List/main.js
@@ -1,123 +1,43 @@
-(function(){
-    // form set 
-    // array object for select item
-const select = [
-    {
-    value:1,
-    select_title:"Great- 20%",
+const itemInput =document.querySelector('#itemInput');
+const formSubmit =document.querySelector('#itemForm')
+const clearlist = document.querySelector('#clear-list');
+const alerttext =document.querySelector('.error');
+console.log(formSubmit+" "+alerttext+itemInput)
+let todoList =[];
 
-    },
-    {
-        value:2,
-        select_title:"Good -10%",
-        
-    },
-    {
-        value:3,
-        select_title:"Bad -5%",
-        
+
+const inputValidate =(input)=>{
+    let flag=false;
+      console.log(input);
+      alerttext.classList.remove('alert','alert-danger')
+      alerttext.textContent="";
+    if(input==''){
+        alerttext.classList.add('alert', 'alert-danger')
+      alerttext.textContent="empty text field please add any value in text field!";
+      flag =true;
     }
-  
-];
+    return flag;
 
-select.forEach(select => {
-    // create option element 
-    const option = document.createElement('option');
-    // set the array object value to the option 
-    option.value=select.value;
-    option.textContent=select.select_title;
-    const selectbox = document.getElementById('selectbox');
-    selectbox.appendChild(option);
-});
+}
 
-// ---------------form setup dyanmically --------------
- 
 
-// form validate 
-const select_input =document.querySelectorAll('.totalbill');
-
-const formValidate=(total_bill,bill_share,select_option)=>{
-    // create paragraph tag
-
-  
- 
-    // function insertAfter(reference_element,new_element){
-    //     reference_element.parentNode.insertBefore(new_element,reference_element.nextSibling);
-    
-    // }
-  
-    
-    let flag =false;
-    const erorr_message =document.querySelectorAll('.error_message');
-console.log(erorr_message);
-erorr_message.forEach((error_message)=>{
-    error_message.style.color="red";
-    error_message.innerHTML="";
+const formHandle =()=>{
+    const todoItems =document.querySelector('.todo-item');
+    todoItems.forEach(todoitem => {
+        if(todoitem.querySelector('.todo-content').textContent==value){
+            document.querySelector('.complete').addEventListener('click',(e)=>{
+                todoitem.querySelector('.todo-content').classList.toggle('completed');
+                this.classList.toggle('visibility');
+            });
+            
+          todoitem.querySelector('.edit').addEventListener('click',()=>{
+            itemInput.textContent=vlaue;
+          })
+        }
+        
+    });
+}
+formSubmit.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    inputValidate(itemInput.value);
 })
-    //   bill validate 
-    if(total_bill==''||total_bill<0){
-         erorr_message[0].innerHTML="field blank or <0!";
-        flag =true;
-
-    }
-    // number of bill share validate 
-    if(bill_share<0 || bill_share==''){
-        erorr_message[1].innerHTML="field blank or <0!"
-        flag =true;
-    }
-    // select field validate 
-    
-    if(select_option<1||select_option>3){
-        erorr_message[2].innerHTML="please choose your service!"
-        flag =true;
-    }
-   
-  return flag  ;
-}
-// calcuate the tip amount 
-const results =(total_bill,bill_share,select_option)=>{
-    let tip_percent='';
-
-    if(select_option==1){
-        tip_percent=.2;
-    }
-    if(select_option==2){
-        tip_percent=.1;
-    }
-    if(select_option==3){
-        tip_percent=.02;
-    }
-    const tip_amount = Number(total_bill)*tip_percent;
-    const total_amount = Number(total_bill)+Number(tip_amount);
-    const per_person = Number(tip_amount)/Number(bill_share);
-return [tip_amount,total_amount,per_person];
-    
-}
-// form sumbit
-const tip_form = document.querySelector('.tip-form');
-tip_form.addEventListener('submit',(e)=>{
-e.preventDefault();
-let total_bill= select_input[0].value;
-let bill_share =select_input[1].value;
-let select_option=select_input[2].value;
-let result = document.querySelector('.result');
-console.log(total_bill+" "+bill_share+" "+select_option)
-const isTrue =formValidate(total_bill,bill_share,select_option)
-if(!isTrue){
-    const finalResult = results(total_bill,bill_share,select_option);
-    const loader = document.querySelector('.loader');
-       loader.classList.add('showloader');
-      
-  setTimeout(()=>{   
-
-    loader.classList.remove('showloader');
-    const resultsshow = document.querySelectorAll('.results');
-    resultsshow[0].textContent=`Total Tip:रु${finalResult[0].toFixed(2)}`;
-    resultsshow[1].textContent=`Total Bill Amount:रु${finalResult[1].toFixed(2)}`;
-    resultsshow[2].textContent=`Per Person:रु${finalResult[2].toFixed(2)}`;
-    result.classList.add('showloader');
-},2000);
-
-}
-});
-})();
